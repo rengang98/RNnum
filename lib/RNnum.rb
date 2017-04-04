f702279c0147a1efda6c6c2b789d3466a5a16f38
rengang98 <86554149@qq.com>
2017-04-04 11:24:15 +0100

    Update RNnum.rb

--- lib/RNnum.rb
+++ lib/RNnum.rb
@@ -1,7 +1,21 @@
 require "RNnum/version"
 
 module RNnum
- class << self
+class << self
+    def generate_password len = 8
+      seed = (0..9).to_a + ('a'..'z').to_a + ('A'..'Z').to_a + ['!', '@', '#', '$', '%', '.', '*'] * 4
+      token = ""
+      len.times { |t| token << seed.sample.to_s }
+      token
+    end
+
+    def generate_utoken len = 8
+      a = lambda { rand(36).to_s(36) }
+      token = ""
+      len.times { |t| token << a.call.to_s }
+      token
+    end
+
     def generate_tplay_id
       Date.today.to_s.split('-')[1..-1].join() << generate_utoken(6).upcase
     end
